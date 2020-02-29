307b1028583c795a0669e096735d6475654faa94
20Bulka20 <polina.rozhko.blr@gmail.com>
2020-02-29 14:34:01 +0300

    feat: add my sollution to Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+// Given: number, your task is to implement function that reverse digits of this number.
+// For example:
+// reverse(123); // 321
+// reverse(233); // 332
+// reverse(535); // 535
+// reverse(95034); // 43059
 module.exports = function reverse (n) {
-  
+    n<0? n=n*(-1)+'' : n=n+'';   
+  return n.split('').reverse().join('');
 }
